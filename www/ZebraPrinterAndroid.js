e7e6382c127cd806d0f97784af40fa8e19923c94
osmanbozkurt02 <47953463+osmanbozkurt02@users.noreply.github.com>
2020-10-16 16:39:02 +0300

    Bug fix

--- www/ZebraPrinterAndroid.js
+++ www/ZebraPrinterAndroid.js
@@ -1,13 +1,13 @@
 var exec = require('cordova/exec');
 
 exports.PrintImage = function (PrinterMac, ImageUrl, success, error) {
-    exec(success, error, 'PrintImage', 'PrintAction', [PrinterMac, ImageUrl ]);
+    exec(success, error, 'PrintImage', 'PrintImage', [PrinterMac, ImageUrl ]);
 };
 
 exports.SendCommandToPrinter = function (PrinterMac, CommandText, success, error) {
-    exec(success, error, 'SendCommandToPrinter', 'SendCommandToPrinter', [PrinterMac, CommandText ]);
+    exec(success, error, 'success, error, 'PrintImage', 'SendCommandToPrinter', [PrinterMac, ImageUrl ]', 'SendCommandToPrinter', [PrinterMac, CommandText ]);
 };
 
 exports.GetPrinterLanguage = function (PrinterMac, success, error) {
-    exec(success, error, 'GetPrinterLanguage', 'GetPrinterLanguage', [PrinterMac ]);
+    exec(success, error, 'success, error, 'PrintImage', 'GetPrinterLanguage', [PrinterMac, ImageUrl ]', 'GetPrinterLanguage', [PrinterMac ]);
 };
