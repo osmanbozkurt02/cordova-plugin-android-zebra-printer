bc2b97c38d5e9f780a1eab22a454f35c3cc55316
osmanbozkurt02 <47953463+osmanbozkurt02@users.noreply.github.com>
2020-10-16 18:06:55 +0300

    Add files via upload

--- www/ZebraPrinterAndroid.js
+++ www/ZebraPrinterAndroid.js
@@ -1,13 +1,21 @@
 var exec = require('cordova/exec');
 
 exports.PrintImage = function (PrinterMac, ImageUrl, success, error) {
-    exec(success, error, 'PrintImage', 'PrintImage', [PrinterMac, ImageUrl ]);
+    exec(success, error, 'ZebraPrinterAndroid', 'PrintImage', [PrinterMac, ImageUrl ]);
 };
 
 exports.SendCommandToPrinter = function (PrinterMac, CommandText, success, error) {
-    exec(success, error, 'success, error, 'PrintImage', 'SendCommandToPrinter', [PrinterMac, ImageUrl ]', 'SendCommandToPrinter', [PrinterMac, CommandText ]);
+    exec(success, error, 'ZebraPrinterAndroid', 'SendCommandToPrinter', [PrinterMac, CommandText ]);
 };
 
 exports.GetPrinterLanguage = function (PrinterMac, success, error) {
-    exec(success, error, 'success, error, 'PrintImage', 'GetPrinterLanguage', [PrinterMac, ImageUrl ]', 'GetPrinterLanguage', [PrinterMac ]);
+    exec(success, error, 'ZebraPrinterAndroid', 'GetPrinterLanguage', [PrinterMac ]);
 };
+
+// exports.SendCommandToPrinter = function (PrinterMac, CommandText, success, error) {
+//     exec(success, error, 'PrintImage', 'SendCommandToPrinter', [PrinterMac, ImageUrl ], 'SendCommandToPrinter', [PrinterMac, CommandText ]);
+// };
+
+// exports.GetPrinterLanguage = function (PrinterMac, success, error) {
+//     exec(success, error, 'PrintImage', 'GetPrinterLanguage', [PrinterMac, ImageUrl ]', 'GetPrinterLanguage', [PrinterMac ]);
+// };
