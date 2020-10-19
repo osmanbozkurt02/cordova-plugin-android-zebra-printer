90e0879ccb50d1827741d96ab007f5b955795f93
osmannbozkurt <47953463+osmannbozkurt@users.noreply.github.com>
2020-10-19 15:46:17 +0300

    Update ZebraPrinterAndroid.java

--- src/android/cordova/plugin/zebra/printer/ZebraPrinterAndroid.java
+++ src/android/cordova/plugin/zebra/printer/ZebraPrinterAndroid.java
@@ -248,7 +248,7 @@ private Connection connection;
             e.printStackTrace();
         }
 
-        final String printerLanguage = "";
+         String printerLanguage = "";
         try {
             printerLanguage = SGD.GET("device.languages", connection);
         } catch (ConnectionException e) {
